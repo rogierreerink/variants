47b647a6f55a130a64c57ec951687102d17ec960
Rogier Reerink <me@rogier.io>
2025-04-16 15:24:41 +0200

    Implemented derive macro and added a struct derive test.

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -1,1 +1,7 @@
+use proc_macro::TokenStream;
+use variants_core::expand::expand;
 
+#[proc_macro_attribute]
+pub fn variants(attr: TokenStream, input: TokenStream) -> TokenStream {
+    expand(attr.into(), input.into()).into()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,40 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use variants_derive::variants;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn derive_struct() {
+        #[variants(Summary, Update)]
+        #[allow(dead_code)]
+        struct Foo {
+            #[variants(include(Summary))]
+            bar: usize,
+            #[variants(include(Summary))]
+            #[variants(include(Update), retype = "Option<{}>")]
+            baz: String,
+            #[variants(include(Update), retype = "Option<Box<{b}{v}>>")]
+            ban: Option<Box<Foo>>,
+        }
+
+        let _ = Foo {
+            bar: 0,
+            baz: "hola, mundo".into(),
+            ban: Some(Box::new(Foo {
+                bar: 1,
+                baz: "hello, world".into(),
+                ban: None,
+            })),
+        };
+        let _ = FooSummary {
+            bar: 0,
+            baz: "hola, mundo".into(),
+        };
+        let _ = FooUpdate {
+            baz: Some("hola, mundo".into()),
+            ban: Some(Box::new(FooUpdate {
+                baz: Some("hello, world".into()),
+                ban: None,
+            })),
+        };
     }
 }
